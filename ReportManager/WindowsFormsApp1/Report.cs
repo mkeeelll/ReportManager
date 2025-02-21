6fd732a41abab7d8ec2bec9eac39a03d6f6b80aa
mr-PeRforator <komputernyjila@gmail.com>
2025-02-21 15:35:38 +0300

    Были добавлены классы

---
+++ ReportManager/WindowsFormsApp1/Report.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace WindowsFormsApp1
+{
+    public class Report
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Report(string title, string content, DateTime creationDate)
+        {
+            Title = title;
+            Content = content;
+            CreationDate = creationDate;
+        }
+    }
+}
